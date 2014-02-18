176c36d82078c243aad4821b7f555804b0f79d00
Don Rowe <rowedonalde@gmail.com>
2014-02-18 00:41:11 -0800

    Allowed window dimensions and frame length to be parameterized with command line args passed to glife

--- glife.c
+++ glife.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 #include <unistd.h>
@@ -12,23 +13,24 @@
 /*
  * Displays game graphically rather than with ASCII
  */
-void gdisplay(int* board, int width, int height, SDL_Renderer* ren) {
+void gdisplay(int* board, int board_w, int board_h, SDL_Renderer* ren,
+    int win_w, int win_h) {
     SDL_Surface* surf;
-    surf = SDL_CreateRGBSurface(0, WINDOW_W, WINDOW_H, 32, 0, 0, 0, 0);
+    surf = SDL_CreateRGBSurface(0, win_w, win_h, 32, 0, 0, 0, 0);
     
     SDL_Rect rect;
-    rect.w = WINDOW_W / width;
-    rect.h = WINDOW_H / height;
+    rect.w = win_w / board_w;
+    rect.h = win_h / board_h;
     
     int i;
     int j;
     
-    for (i = 0; i < height; i++) {
+    for (i = 0; i < board_h; i++) {
         // vertical position for this row:
         rect.y = i * rect.h;
-        for (j = 0; j < width; j++) {
+        for (j = 0; j < board_w; j++) {
             // Only try to draw if it's a living cell:
-            if (board[i * width + j] == ALIVE) {
+            if (board[i * board_w + j] == ALIVE) {
                 // Horizontal position of this cell:
                 rect.x = j * rect.w;
                 int r = 255;
@@ -48,6 +50,21 @@ void gdisplay(int* board, int width, int height, SDL_Renderer* ren) {
 }
 
 int main(int argc, char** argv) {
+    // Process args:
+    int wait_ms = 1000;
+    int win_width = WINDOW_W;
+    int win_height = WINDOW_H;
+    if (argc == 2) {
+        wait_ms = atoi(argv[1]);
+    } else if (argc == 3) {
+        win_width = atoi(argv[1]);
+        win_height = atoi(argv[2]);
+    } else if (argc == 4) {
+        wait_ms = atoi(argv[1]);
+        win_width = atoi(argv[2]);
+        win_height = atoi(argv[3]);
+    }
+    
     // Game setup:
     int width;
     int height;
@@ -66,8 +83,8 @@ int main(int argc, char** argv) {
         "Conway's Game of Life",
         100,
         100,
-        500,
-        500,
+        win_width,
+        win_height,
         SDL_WINDOW_SHOWN
     );
     SDL_Renderer* ren = SDL_CreateRenderer(
@@ -77,24 +94,24 @@ int main(int argc, char** argv) {
     );
     
     // Initial frame:
-    gdisplay(even, width, height, ren);
+    gdisplay(even, width, height, ren, win_width, win_height);
     last_frame_time = clock();
     
     // Main Animation Loop:
     int gamerun = 1;
     int tick = 0;
     while (gamerun) {
-        if (clock() - last_frame_time >= 1000000) {
+        if (clock() - last_frame_time >= 1000 * wait_ms) {
             // The order here is flipped around compared to the
             // ascii version. Here, tick represents which tick
             // we're on, not how many we've finished.
             tick++;
             if (tick % 2) {
                 play(even, odd, width, height);
-                gdisplay(odd, width, height, ren);
+                gdisplay(odd, width, height, ren, win_width, win_height);
             } else {
                 play(odd, even, width, height);
-                gdisplay(even, width, height, ren);
+                gdisplay(even, width, height, ren, win_width, win_height);
             }
             last_frame_time = clock();
         }
